05ab3523a4708a7fb2f1134f01ccfafebe0fd019
magdalenewe <delriomagdalene@tuta.io>
2025-08-28 19:35:20 -0300

    lineasgit add .

--- main.c
+++ main.c
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <raylib.h>
+#include <stdbool.h>
 
 void randomArray(int list[], int size){
 	for (int i=0;i < size; i++){
 		list[i]=(rand() % (size+1));
 		}
 }
+void imprimirArray(){
+			for(i=1;i<size;i++){
+								DrawRectangle(i, 1, 1, list[i], RAYWHITE);
+			}
+}
 
 int main(){
 		//setting array
 		srand(time(NULL));
 
-		int size;int i;
+		const int size=800;int i;
 		printf("insertando tamaño\n");
-		scanf("%d", &size);
+		//scanf("%d", &size);
 		if(size <= 0)return 1;
 		printf("%d\n", size);
 
@@ -23,17 +29,20 @@ int main(){
 		randomArray(list, size);
 		
 		//el resto
-		const int screenX =800; const int screenY=600;
-		InitWindow(screenX, screenY, "segmentation fault");
+		const int screenY=600;
+		InitWindow(size, screenY, "segmentation fault");
 
 		SetTargetFPS(60);
 		
 		while (!WindowShouldClose()){
 				BeginDrawing();
+				ClearBackground(BLACK);
+				
+				//while(int j=1;clear=false
+						for(i=1;i<size;i++){
+										DrawRectangle(i, 1, 1, list[i], RAYWHITE);
+						}
 
-						ClearBackground(RAYWHITE);
-
-						DrawText("lol lmao\n",190, 200, 20, LIGHTGRAY);
 				EndDrawing();
 		}
 		CloseWindow();
